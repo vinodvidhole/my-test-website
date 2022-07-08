bb553448ff720b7d09ec1aa15c7831c48018aadb
Vinod <37751567+vinodvidhole@users.noreply.github.com>
2022-07-08 04:31:15 +0000

    made some changes

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template
+import requests
+import json
 
 app = Flask(__name__)
-  
+
+
+git_hub_projects = "https://api.github.com/users/vinodvidhole/repos"
+response = requests.get(git_hub_projects)
+
+if response.ok :
+  project_data = json.loads(response.text)
+
+#print(project_data)
 @app.route('/')
 def hello_world():
   return render_template('index.html')
