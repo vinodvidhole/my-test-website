39994c84e1e0d9ec133f5bce7b5fc290efb28d54
Vinod <37751567+vinodvidhole@users.noreply.github.com>
2022-07-08 05:29:11 +0000

    dynamic projects

--- app.py
+++ app.py
@@ -1,20 +1,13 @@
 from flask import Flask, render_template
-import requests
-import json
+from getdata import get_projects
 
 app = Flask(__name__)
 
-
-git_hub_projects = "https://api.github.com/users/vinodvidhole/repos"
-response = requests.get(git_hub_projects)
-
-if response.ok :
-  project_data = json.loads(response.text)
-
-#print(project_data)
 @app.route('/')
 def hello_world():
-  return render_template('index.html')
+  project_list = get_projects()
+  return render_template('index.html',
+                        project_list=project_list)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
---
+++ getdata.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+def get_projects():
+  git_hub_projects = "https://api.github.com/users/vinodvidhole/repos"
+  response = requests.get(git_hub_projects)
+  
+  if response.ok :
+    project_data = json.loads(response.text)
+  
+  project_list = []
+  for project in project_data:
+      topic_list = []
+      project_dict = {}
+      topic_list = project["topics"]
+      if len(topic_list) > 0 :
+          topic_str = project["topics"][0] 
+          if topic_str[:7] == "project":
+              project_dict["html_url"] = project["html_url"]
+              project_dict["description"] = project["description"]
+              project_dict["name"] = project["name"]
+              project_dict["filter"] = topic_str[8:]
+              project_list.append(project_dict)
+  return project_list
+  
